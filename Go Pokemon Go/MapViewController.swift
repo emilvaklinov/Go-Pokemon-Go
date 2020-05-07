fdc003f8dc50cf7765a9cdc9017d4bf840ea8629
emil vaklinov <emil_vaklinov@yahoo.com>
2020-05-07 22:45:53 +0100

    Outlets added

--- Go Pokemon Go/MapViewController.swift
+++ Go Pokemon Go/MapViewController.swift
@@ -7,24 +7,18 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
+    @IBOutlet weak var centerTapped: UIButton!
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
--- Go Pokemon Go/PokedexViewController.swift
+++ Go Pokemon Go/PokedexViewController.swift
@@ -9,22 +9,16 @@
 import UIKit
 
 class PokedexViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancelTapped(_ sender: Any) {
+    dismiss(animated: true, completion: nil)
     }
-    */
-
+    
 }
