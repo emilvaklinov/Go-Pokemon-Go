5d44931bae4dc7d6fc3e4c9c43addc6a17e8082c
emil vaklinov <emil_vaklinov@yahoo.com>
2020-05-08 23:24:01 +0100

    action completed

--- Go Pokemon Go/Controllers/PokedexViewController.swift
+++ Go Pokemon Go/Controllers/PokedexViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class PokedexViewController: UIViewController {
+class PokedexViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
     
     var caughtPokemon : [Pokemon] = []
@@ -16,12 +16,54 @@ class PokedexViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        caughtPokemon = getPokemon(caught: true)
+        uncaughtPokemon = getPokemon(caught: false)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "CAUGHT"
+        } else {
+            return "UNCAUGHT"
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return caughtPokemon.count
+        } else {
+            return uncaughtPokemon.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        var pokemon : Pokemon
+        if indexPath.section == 0 {
+            pokemon = caughtPokemon[indexPath.row]
+        } else {
+            pokemon = uncaughtPokemon[indexPath.row]
+        }
+        
+        cell.textLabel?.text = pokemon.name
+        
+        if let imageName = pokemon.imageName {
+            cell.imageView?.image = UIImage(named: imageName)
+        }
+        
+        return cell
+    }
     @IBAction func cancelTapped(_ sender: Any) {
-    dismiss(animated: true, completion: nil)
+        dismiss(animated: true, completion: nil)
     }
     
 }
--- Go Pokemon Go/Helpers/CoreDataHelper.swift
+++ Go Pokemon Go/Helpers/CoreDataHelper.swift
@@ -10,13 +10,13 @@ import UIKit
 import CoreData
 
 func addAllPokemon() {
-    createPokemon(name: "Pikachu", imageName: "pickachu-2")
     createPokemon(name: "Mankey", imageName: "mankey")
+    createPokemon(name: "Pikachu", imageName: "pikachu-2")
     createPokemon(name: "Psyduck", imageName: "psyduck")
     createPokemon(name: "Snorlax", imageName: "snorlax")
 }
 
-func createPokemon(name: String, imageName: String) {
+func createPokemon(name:String, imageName:String) {
     if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
         let pokemon = Pokemon(context: context)
         pokemon.imageName = imageName
@@ -24,18 +24,33 @@ func createPokemon(name: String, imageName: String) {
         try? context.save()
     }
 }
-    
-    func getAllPokemon() -> [Pokemon] {
-        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
-            let pokemonData = try? context.fetch(Pokemon.fetchRequest()) as? [Pokemon]
-            if let pokemons = pokemonData {
-                if pokemons.count == 0 {
-                    addAllPokemon()
-                    return getAllPokemon()
-                } else {
-                    return pokemons
-                }
+
+func getAllPokemon() -> [Pokemon] {
+    if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+        let pokemonData = try? context.fetch(Pokemon.fetchRequest()) as? [Pokemon]
+        if let pokemons = pokemonData {
+            if pokemons.count == 0 {
+                addAllPokemon()
+                return getAllPokemon()
+            } else {
+                return pokemons
             }
         }
-        return []
+    }
+    return []
+}
+
+func getPokemon(caught:Bool) -> [Pokemon] {
+    if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+        let fetchRequest = Pokemon.fetchRequest() as NSFetchRequest<Pokemon>
+        if caught {
+            fetchRequest.predicate = NSPredicate(format: "caught == true")
+        } else {
+            fetchRequest.predicate = NSPredicate(format: "caught == false")
+        }
+        if let pokemons = try? context.fetch(fetchRequest) {
+            return pokemons
+        }
+    }
+    return []
 }
