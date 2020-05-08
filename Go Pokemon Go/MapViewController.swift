70f636889da74e684cd40669208512853796f9d5
emil vaklinov <emil_vaklinov@yahoo.com>
2020-05-08 13:22:42 +0100

    Location manager added with tracking the user when is moving.

--- Go Pokemon Go/MapViewController.swift
+++ Go Pokemon Go/MapViewController.swift
@@ -9,16 +9,72 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
+    var manager = CLLocationManager()
+    var updateCount = 0
+    
     @IBOutlet weak var mapView: MKMapView!
+  
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        manager.delegate = self
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            setup()
+        }else{
+            manager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            setup()
+        }
+    }
+    
+    // Showing the users location
+    func setup() {
+        mapView.showsUserLocation = true
+        manager.stopUpdatingLocation()
+        mapView.delegate = self
+    }
+    // Custom annotation
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let annoView = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        if annotation is MKUserLocation {
+            annoView.image = UIImage(named: "player")
+            var frame = annoView.frame
+            frame.size.height = 50.0
+            frame.size.width = 50.0
+            annoView.frame = frame
+            
+            
+        }
+        return annoView
+    }
+    
+// To follow the location of the user when is moving
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+       
+        if updateCount < 3 {
+            if let center = manager.location?.coordinate {
+                let region = MKCoordinateRegion(center: center, latitudinalMeters: 200, longitudinalMeters: 200)
+                mapView.setRegion(region, animated: false)
+            }
+            updateCount += 1
+        } else {
+            manager.stopUpdatingLocation()
+        }
 
     }
-    @IBOutlet weak var centerTapped: UIButton!
     
-
-  
-
+    @IBAction func centerTapped(_ sender: Any) {
+        if let center = manager.location?.coordinate {
+            let region = MKCoordinateRegion(center: center, latitudinalMeters: 200, longitudinalMeters: 200)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
 }
